9dc13f3023051cc9c92f15ab7d1dcdd6882d8050
murshidazher <murshidazher@yahoo.com>
2021-10-19 13:20:08 +0530

    :sparkles: feat: update the terminal colors

--- setup/lib.sh
+++ setup/lib.sh
@@ -10,7 +10,10 @@ COL_RESET=$ESC_SEQ"39;49;00m"
 COL_RED=$ESC_SEQ"31;01m"
 COL_GREEN=$ESC_SEQ"32;01m"
 COL_YELLOW=$ESC_SEQ"33;01m"
-COL_BLUE=$ESC_SEQ"\e[96m"
+COL_BLUE=$ESC_SEQ"34;01m"
+COL_PURPLE=$ESC_SEQ"35;01m"
+COL_CYAN=$ESC_SEQ"36;01m"
+COL_WHITE=$ESC_SEQ"37;01m"
 
 function ok() {
   echo -e "$COL_GREEN[ok]$COL_RESET $1"
@@ -65,7 +68,7 @@ function awesome_header() {
 }
 
 function ask_for_confirmation() {
-  echo -e "\e[1m$1\e[0m (y/N) "
+  echo -e "$COL_PURPLE[confirmation]$COL_RESET $1 (y/N) "
   read -n 1
   echo -e "\n"
 }

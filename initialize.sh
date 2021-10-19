201f3559edf0035842373c2c140b7200187c58f7
murshidazher <murshidazher@yahoo.com>
2021-10-19 13:48:11 +0530

    :sparkles: feat: update the initialize file for cloning

--- initialize.sh
+++ initialize.sh
@@ -164,93 +164,93 @@ ask_for_sudo
 # Create a file to log m1 agnostic binaries
 touch $HOME/installation_setup.log
 
-# Setup macbook name
-ask_for_confirmation "Do you need to change your macbook name?"
-if answer_is_yes; then
-  read -p 'Input your new macbook name: ' mbname
-  sudo scutil --set ComputerName "$mbname"
-  sudo scutil --set LocalHostName "$mbname"
-  sudo scutil --set HostName "$mbname"
-  dscacheutil -flushcache
-else
-  cancelled "Ok, let's continue".
-fi
+# # Setup macbook name
+# ask_for_confirmation "Do you need to change your macbook name?"
+# if answer_is_yes; then
+#   read -p 'Input your new macbook name: ' mbname
+#   sudo scutil --set ComputerName "$mbname"
+#   sudo scutil --set LocalHostName "$mbname"
+#   sudo scutil --set HostName "$mbname"
+#   dscacheutil -flushcache
+# else
+#   cancelled "Ok, let's continue".
+# fi
 
-# Download all available macos updates.
-action "Download Mac updates:\n"
-sudo softwareupdate -d -a
+# # Download all available macos updates.
+# action "Download Mac updates:\n"
+# sudo softwareupdate -d -a
 
-# Install all available macos updates.
-action "Installing Mac updates:\n"
-sudo softwareupdate -iaR
+# # Install all available macos updates.
+# action "Installing Mac updates:\n"
+# sudo softwareupdate -iaR
 
-#-------------------------------------------
-# Prerequisite: Login to Github
-# Generate ssh keys & add to ssh-agent
-# See: https://help.github.com/articles/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent/
-#-------------------------------------------
+# #-------------------------------------------
+# # Prerequisite: Login to Github
+# # Generate ssh keys & add to ssh-agent
+# # See: https://help.github.com/articles/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent/
+# #-------------------------------------------
 
-running "Generating ssh keys, adding to ssh-agent... \n"
-read -p 'Input email for ssh key: ' useremail
+# running "Generating ssh keys, adding to ssh-agent... \n"
+# read -p 'Input email for ssh key: ' useremail
 
-running "Use default ssh file location, enter a passphrase: \n"
-ssh-keygen -t rsa -b 4096 -C "$useremail" # will prompt for password
-eval "$(ssh-agent -s)"
+# running "Use default ssh file location, enter a passphrase: \n"
+# ssh-keygen -t rsa -b 4096 -C "$useremail" # will prompt for password
+# eval "$(ssh-agent -s)"
 
-# Now that sshconfig is synced add key to ssh-agent and
-# store passphrase in keychain
-ssh-add -K ~/.ssh/id_rsa
-success "ssh identity has been added."
+# # Now that sshconfig is synced add key to ssh-agent and
+# # store passphrase in keychain
+# ssh-add -K ~/.ssh/id_rsa
+# success "ssh identity has been added."
 
-# If you're using macOS Sierra 10.12.2 or later, you will need to modify your ~/.ssh/config file to automatically load keys into the ssh-agent and store passphrases in your keychain.
+# # If you're using macOS Sierra 10.12.2 or later, you will need to modify your ~/.ssh/config file to automatically load keys into the ssh-agent and store passphrases in your keychain.
 
-if [ -e ~/.ssh/config ]; then
-  cancelled "ssh config already exists. Skipping adding osx specific settings... "
-else
-  success "Writing osx specific settings to ssh config... "
-  cat <<EOT >>~/.ssh/config
-	Host *
-		AddKeysToAgent yes
-		UseKeychain yes
-		IdentityFile ~/.ssh/id_rsa
-EOT
-fi
+# if [ -e ~/.ssh/config ]; then
+#   cancelled "ssh config already exists. Skipping adding osx specific settings... "
+# else
+#   success "Writing osx specific settings to ssh config... "
+#   cat <<EOT >>~/.ssh/config
+# 	Host *
+# 		AddKeysToAgent yes
+# 		UseKeychain yes
+# 		IdentityFile ~/.ssh/id_rsa
+# EOT
+# fi
 
-#-------------------------------------------
-# Add ssh-key to GitHub via api
-#-------------------------------------------
+# #-------------------------------------------
+# # Add ssh-key to GitHub via api
+# #-------------------------------------------
 
-action "Adding ssh-key to GitHub (via api)..."
-warn "Important! For this step, use a github personal token with the admin:public_key permission."
-actioninfo "If you don't have one, create it here: https://github.com/settings/tokens/new"
+# action "Adding ssh-key to GitHub (via api)..."
+# warn "Important! For this step, use a github personal token with the admin:public_key permission."
+# actioninfo "If you don't have one, create it here: https://github.com/settings/tokens/new"
 
-retries=3
-SSH_KEY=$(cat ~/.ssh/id_rsa.pub)
+# retries=3
+# SSH_KEY=$(cat ~/.ssh/id_rsa.pub)
 
-for ((i = 0; i < retries; i++)); do
-  read -p 'GitHub username: ' ghusername
-  read -p 'Machine name: ' ghtitle
-  read -sp 'GitHub personal token: ' ghtoken
+# for ((i = 0; i < retries; i++)); do
+#   read -p 'GitHub username: ' ghusername
+#   read -p 'Machine name: ' ghtitle
+#   read -sp 'GitHub personal token: ' ghtoken
 
-  gh_status_code=$(curl -o /dev/null -s -w "%{http_code}\n" -u "$ghusername:$ghtoken" -d '{"title":"'$ghtitle'","key":"'"$SSH_KEY"'"}' 'https://api.github.com/user/keys')
+#   gh_status_code=$(curl -o /dev/null -s -w "%{http_code}\n" -u "$ghusername:$ghtoken" -d '{"title":"'$ghtitle'","key":"'"$SSH_KEY"'"}' 'https://api.github.com/user/keys')
 
-  if (($gh_status_code - eq == 201)); then
-    success "GitHub ssh key added successfully!"
-    break
-  else
-    error "Something went wrong. Enter your credentials and try again..."
-    error -n "Status code returned: "
-    error $gh_status_code
-  fi
-done
+#   if (($gh_status_code - eq == 201)); then
+#     success "GitHub ssh key added successfully!"
+#     break
+#   else
+#     error "Something went wrong. Enter your credentials and try again..."
+#     error -n "Status code returned: "
+#     error $gh_status_code
+#   fi
+# done
 
-[[ $retries -eq i ]] && botintro "Adding ssh-key to GitHub failed! Try again later."
+# [[ $retries -eq i ]] && botintro "Adding ssh-key to GitHub failed! Try again later."
 
 #-------------------------------------------
 # Install dotfiles repo, run link script
 #-------------------------------------------
 
-if [! -d "$HOME/dev/src/github" ]; then
+if [ ! -d "$HOME/dev/src/github" ]; then
   mkdir -p $HOME/dev/src/github
   success "Create a dev directory on root"
 else

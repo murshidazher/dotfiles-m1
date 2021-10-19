371ebebd114a61299048f0badfc745c2d23a572a
murshidazher <murshidazher@yahoo.com>
2021-10-19 14:34:08 +0530

    :sparkles: feat: add backup confirmation

--- setup/backup.sh
+++ setup/backup.sh
@@ -33,35 +33,41 @@ if $dirsuccess; then
   success "Backup directories created."
 else
   error "Errors when creating backup directories, please check and resolve."
-  cancelled "\e[1mCannot proceed. Exit.\e[0m"
+  cancelled "Cannot proceed. Exit."
   exit -1
 fi
 
-action "Backup directories"
-# Loop array of directories that dotfiles handles, copy them to backup
-for i in "${dotfilesdirarray[@]}"; do
-  cp -Rp ${i/$dotfilesdir/$HOME} "$dotfilesbackupdir"
-  print_result $? "Copying ${i/$dotfilesdir/$HOME}"
-done
-
-action "Backup files"
-# Loop array of directories that dotfiles handles files for
-for i in "${dotfilesfilearray[@]}"; do
-  declare -a tmparr=()
-
-  # Properly store the results of find on these directories in an array
-  # https://stackoverflow.com/questions/23356779/how-can-i-store-find-command-result-as-arrays-in-bash
-  # We want to handle .*, *.cfg, *.conf and NOT .DS_Store, .git, .osx, .macos and no *.sh files
-  while IFS= read -r -d $'\0'; do
-    tmparr+=("$REPLY")
-  done < <(find "$i" -type f -maxdepth 1 \( -name ".*" -o -name "*.cfg" -o -name "*.conf" \) -a -not -name .DS_Store -not -name .git -not -name .osx -not -name .macos -not -name "*.sh" -print0)
-
-  # For each match file in each directory, copy that to backup
-  for j in "${tmparr[@]}"; do
-    cp -Rp ${j/$i/$HOME} "$dotfilesbackupdir"
-    print_result $? "Copying ${j/$i/$HOME}"
+# Backup everything
+ask_for_confirmation "Do you need to backup everything ?"
+if answer_is_yes; then
+  action "Backup directories"
+  # Loop array of directories that dotfiles handles, copy them to backup
+  for i in "${dotfilesdirarray[@]}"; do
+    cp -Rp ${i/$dotfilesdir/$HOME} "$dotfilesbackupdir"
+    print_result $? "Copying ${i/$dotfilesdir/$HOME}"
   done
-done
+
+  action "Backup files"
+  # Loop array of directories that dotfiles handles files for
+  for i in "${dotfilesfilearray[@]}"; do
+    declare -a tmparr=()
+
+    # Properly store the results of find on these directories in an array
+    # https://stackoverflow.com/questions/23356779/how-can-i-store-find-command-result-as-arrays-in-bash
+    # We want to handle .*, *.cfg, *.conf and NOT .DS_Store, .git, .osx, .macos and no *.sh files
+    while IFS= read -r -d $'\0'; do
+      tmparr+=("$REPLY")
+    done < <(find "$i" -type f -maxdepth 1 \( -name ".*" -o -name "*.cfg" -o -name "*.conf" \) -a -not -name .DS_Store -not -name .git -not -name .osx -not -name .macos -not -name "*.sh" -print0)
+
+    # For each match file in each directory, copy that to backup
+    for j in "${tmparr[@]}"; do
+      cp -Rp ${j/$i/$HOME} "$dotfilesbackupdir"
+      print_result $? "Copying ${j/$i/$HOME}"
+    done
+  done
+else
+  cancelled "Ok, let's only backup the local directories and files".
+fi
 
 action "Backup other local directories and files (just incase...)\n"
 # Copy other misc $HOME files
@@ -77,4 +83,4 @@ cp -Rp ~/.ssh "$dotfilesbackupdir"
 cp -Rp ~/.z "$dotfilesbackupdir"
 
 echo -en "\n"
-success "\e[1mBackup completed to $HOME/backup/dotfiles-backup\e[0m"
+success "Backup completed to $HOME/backup/dotfiles-backup"

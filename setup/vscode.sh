3de1f9b12d360d9740af524d7fdcceec8054014e
murshidazher <murshid@different.com.au>
2021-11-21 15:56:13 +0530

    :wrench: chore: add remove editor

--- setup/vscode.sh
+++ setup/vscode.sh
@@ -24,3 +24,6 @@ code --list-extensions | comm -13 - $defaultdotfilesdir/vscode/extensions.list |
 
 # add vscode as default commit editor
 git config --global core.editor "code --wait"
+
+# to remove the editor
+# git config --unset core.editor
